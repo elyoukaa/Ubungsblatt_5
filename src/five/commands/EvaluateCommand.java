ccdabb6b8a31707ae354643a8a9dc3f68a2babd8
lukaw <luka.wu@gmx.de>
2024-01-31 22:01:08 +0100

    uuxxo

--- src/five/commands/EvaluateCommand.java
+++ src/five/commands/EvaluateCommand.java
@@ -50,6 +50,7 @@ public class EvaluateCommand implements Command {
         if (theftS > 0 && eddie.getGodPower() > 0) {
             sam.changeGP(Math.min(theftS, eddie.getGodPower()));
             eddie.changeGP(Math.min(theftS, eddie.getGodPower()) * (-1));
+            System.out.println("ZUI");
         } else if (theftE > 0 && sam.getGodPower() > 0) {
             eddie.changeGP(Math.min(theftE, sam.getGodPower()));
             sam.changeGP(Math.min(theftE, sam.getGodPower()) * (-1));
