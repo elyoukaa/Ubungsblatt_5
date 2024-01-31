d63352b2bd7b365a176af834c01cc7b20e7c2253
lukaw <luka.wu@gmx.de>
2024-01-31 19:43:14 +0100

    uuxxo

--- src/five/Main.java
+++ src/five/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         CommandHandler commandHandler = new CommandHandler();
+        commandHandler.handleUserInput();
     }
 }
